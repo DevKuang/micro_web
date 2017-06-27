a22a0ea8c1e060427ef1fda28bd6f0e23bd019de
DevKuang <DevKuang@foxmail.com>
2017-06-28 02:53:32 +0800

    for heroku

---
+++ app/main/forms.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,TextAreaField,BooleanField,SelectField
+from wtforms.validators import DataRequired,Length,Email,Regexp
+from wtforms import ValidationError
+from flask_pagedown.fields import PageDownField
+from ..models import Role,User,PostClassification
+
+
+class NameForm(FlaskForm):
+    name = StringField(u'你的名字是什么?', validators=[DataRequired()])
+    submit = SubmitField(u'提交')
+
+
+class EditProfileForm(FlaskForm):
+    name=StringField(u'真实姓名',validators=[Length(0,64)])
+    location=StringField(u'住址',validators=[Length(0,64)])
+    about_me=TextAreaField(u'个人资料')
+    submit=SubmitField(u'提交')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email=StringField(u'邮箱',validators=[DataRequired(),Length(1,64),Email])
+    username=StringField(u'用户名',validators=[DataRequired(),Length(1,64),Regexp('^[A-Za-z0-9_.]*$',0,
+                                                                               'Usernames must have only letters,'
+                                                                               'numbers,dots or underscores'
+                                                                               )])
+    confirmed=BooleanField(u'邮箱是否验证')
+    role=SelectField(u'角色',coerce=int)
+    name=StringField(u'真实姓名',validators=[Length(0,64)])
+    location=StringField(u'位置',validators=[Length(0,64)])
+    about_me=StringField(u'关于我')
+    submit=SubmitField(u'提交')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self,field):
+        if field.data!=self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已被占用')
+
+    def validate_username(self,field):
+        if field.data!=self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户名已被占用')
+
+
+class PostForm(FlaskForm):
+    title=StringField(u'标题',validators=[DataRequired()])
+    body=PageDownField(u'内容')
+    submit=SubmitField(u'提交')
+
+
+class CreatePostForm(FlaskForm):
+    postclassification=SelectField(u'文章类别',coerce=int)
+    title=StringField(u'标题',validators=[DataRequired()])
+    body=PageDownField(u'内容(支持Markdown)')
+    submit=SubmitField(u'提交文章')
+
+    def __init__(self,*args,**kwargs):
+        super(CreatePostForm, self).__init__(*args,**kwargs)
+        self.postclassification.choices=[(postclassification.id,postclassification.name)
+                                     for postclassification in PostClassification.query.order_by(PostClassification.name).all()]
+
+
+class CommentForm(FlaskForm):
+    body=StringField(u'输入你的评论',validators=[DataRequired()])
+    submit=SubmitField(u'提交')
---
+++ config.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '18826432982@163.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    BLOG_MAIL_SUBJECT_PREFIX = '[Blog]'
+    BLOG_MAIL_SENDER = 'Blog Admin <18826432982@163.com>'
+    BLOG_ADMIN = 'devkuang@foxmail.com'
+    BLOG_POSTS_PER_PAGE=15
+    BLOG_FOLLOWERS_PER_PAGE=20
+    BLOG_COMMENTS_PER_PAGE = 30
+    BLOG_SLOW_DB_QUERY_TIME = 0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls,app):
+        Config.init_app(app)
+
+
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials=None
+        secure=None
+        if getattr(cls,'MAIL_USERNAME',None) is not None:
+            credentials=(cls.MAIL_USERNAME,cls.MAIL_PASSWORD)
+            if getattr(cls,'MAIL_USE_TLS',None):
+                secure=()
+        mail_handler=SMTPHandler(
+            mailhost=(cls.MAIL_SERVER,cls.MAIL_PORT),
+            fromaddr=cls.BLOG_MAIL_SENDER,
+            toaddrs=[cls.BLOG_ADMIN],
+            subject=cls.BLOG_MAIL_SUBJECT_PREFIX+'Applicaton Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+class HerokuConfig(ProductionConfig):
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        # handle proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app=ProxyFix(app.wsgi_app)
+
+        #log to stdeer
+        import logging
+        from logging import StreamHandler
+        file_handler=StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'heroku':HerokuConfig,
+
+    'default': DevelopmentConfig
+}
